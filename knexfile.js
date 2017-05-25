2d8fd038d7d1026050c4f5bf91a78a8901ad8b7e
Mitchell Porter <mitchellporter@gmail.com>
2017-05-25 12:34:39 -0700

    created knexfile

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: process.env.DEVELOPMENT_DATABASE_URL,
+        pool: {
+            min: 0,
+            max: 10,
+            afterCreate: function(conn, cb) {
+                conn.query('SET timezone="UTC";', function(err) {
+                    if (err) {
+                        cb(err, conn);
+                    } else {
+                        conn.query('select 1+1 as result;', function(err) {
+                            cb(err, conn);
+                        });
+                    }
+                });
+            }
+        }
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.PRODUCTION_DATABASE_URL,
+        pool: {
+            min: 0,
+            max: 10,
+            afterCreate: function(conn, cb) {
+                conn.query('SET timezone="UTC";', function(err) {
+                    if (err) {
+                        cb(err, conn);
+                    } else {
+                        conn.query('select 1+1 as result;', function(err) {
+                            cb(err, conn);
+                        });
+                    }
+                });
+            }
+        }
+    }
+};
